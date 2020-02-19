f7c1447e8fd87debad433891f663841ecb123e2d
Omkar <omkarvd7066@gmail.com>
2020-02-19 15:54:27 +0530

    Added Projects

---
+++ Jarvis/jarvis.py
@@ -0,0 +1,38 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+#print(voices)
+engine.setProperty('voice',voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning Omkar!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Evening")
+
+
+    speak("I am Jarvis.  Please tell me  how may I help you!")       
+
+def takeCommand():
+    #It takes mocrophone input from user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+if __name__ == "__main__":
+    wishMe()
+
+
